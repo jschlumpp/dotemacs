529b79c86ecc1c6b3f47ff1a2820cf36d17bdf3e
Julia Schlumpp <julia@unikraft.io>
2026-07-14 09:56:34 +0200

    Add markdown and cc-mode config
    Signed-off-by: Julia Schlumpp <julia@unikraft.io>

--- lisp/init-lang.el
+++ lisp/init-lang.el
@@ -2,4 +2,15 @@
   :straight (:host github :repo "NixOS/nix-mode")
   :mode ("\\.nix\\'" . nix-mode))
 
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+              ("C-c C-e" . markdown-do)))
+
+(use-package cc-mode
+  :straight nil
+  :hook (c-mode-hook . electric-pair-local-mode))
+
 (provide 'init-lang)
